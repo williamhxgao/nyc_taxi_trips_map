91e98b8c98db8f55380d143b285d71d3c89e079b
williamhxgao <ghx5055@yahoo.com.au>
2021-04-12 14:13:46 +1000

    Add env vars, put data fetch in a separete file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, {Component, useState} from "react";
+import React, {useState} from "react";
 import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
-import NYCTripsReader  from "./NYCTripsReader"
-
-//require('dotenv').config();
-
+import NYCTripsReader from "./NYCTripsReader";
 
 function Map(){
 
+  let map = null; 
+  
+  var DataFetch = NYCTripsReader;
+
+  const[isGroup, setIsGroup] = useState(true);
+  
   const[data, setDate] = useState([]);
 
   const [selectedMark, setSelectedMark] = useState(null);
 
-  let map = null;
+  
 
   return(
     <GoogleMap 
@@ -20,46 +23,48 @@ function Map(){
       defaultCenter={{lat:40.712776, lng:-74.005974}} 
       onTilesLoaded={
         () =>{
-
+          var sw = {lat: map.getBounds().getSouthWest().lat(),lng: map.getBounds().getSouthWest().lng()};
+          var ne = {lat: map.getBounds().getNorthEast().lat(),lng: map.getBounds().getNorthEast().lng()}; 
+          DataFetch(isGroup, sw, ne, "PickUp").then(data => setDate(data));
         }
         
       }
       onZoomChanged={
         () =>{
             var sw = {lat: map.getBounds().getSouthWest().lat(),lng: map.getBounds().getSouthWest().lng()};
-            var ne = {lat: map.getBounds().getNorthEast().lat(),lng: map.getBounds().getNorthEast().lng()};
-            console.log("sw" + sw.lat + ";" + sw.lng);
-            console.log("ne" + ne.lat + ";" + ne.lng);  
-
-            fetch("https://localhost:5001/api/TaxiTrips/groups/" + sw.lng + "/" + (ne.lng - sw.lng) + "/" + sw.lat + "/" + (ne.lat - sw.lat) + "/PickUp" ,
-              {
-                  method:"get",
-                  headers:{
-                     'Accept': 'application/json',
-                     'crossDomain':'true',
-                     'Content-Type': 'application/json',
-                     'Pragma': 'no-cache'
-                 }
-              })
-         .then(response => response.json())
-         .then(data => setDate(data));
-          console.log(data);
+            var ne = {lat: map.getBounds().getNorthEast().lat(),lng: map.getBounds().getNorthEast().lng()}; 
+            setIsGroup(map.getZoom() < 20)
+            DataFetch(isGroup, sw, ne, "PickUp").then(data => setDate(data));
         }
       }
     >
       
-      {data.map(data =>{
-        return(
-        <Marker 
-          position={{
-            lat:data.centre_pickup_latitude,
-            lng:data.centre_pickup_longitude
-          }}
-          onClick={() =>{
-            setSelectedMark(data);
-          }}
-        />);
-      })}
+      { isGroup?
+        data.map(data =>{
+          return(
+          <Marker 
+            position={{
+              lat:data.centre_pickup_latitude,
+              lng:data.centre_pickup_longitude
+            }}
+            onClick={() =>{
+              setSelectedMark(data);
+            }}
+          />);
+        }):
+        data.map(data =>{
+          return(
+          <Marker 
+            position={{
+              lat:data.pickup_latitude,
+              lng:data.pickup_longitude
+            }}
+            onClick={() =>{
+              setSelectedMark(data);
+            }}
+          />);
+        })
+      }
 
       {selectedMark && (
         <InfoWindow
@@ -71,12 +76,22 @@ function Map(){
           setSelectedMark(null);
         }}
         >
-          <div>
-              <h2>Taxi Trips Group {selectedMark.id}</h2>
-              <p>Average Passenger Count: {selectedMark.average_passenger_count}</p>
-              <p>Average Trip Distance: {selectedMark.average_distance}</p>
-              <p>Average Total Amount: {selectedMark.average_total_amount}</p>
-          </div>
+          {
+            isGroup?
+            <div>
+                <h2>Taxi Trips Group: {selectedMark.id}</h2>
+                <p>Number of Trips: {selectedMark.trips}</p>
+                <p>Average Passenger Count: {selectedMark.average_passenger_count}</p>
+                <p>Average Trip Distance: {selectedMark.average_distance}</p>
+                <p>Average Total Amount: {selectedMark.average_total_amount}</p>
+            </div>:
+            <div>
+            <h2>Taxi Trip {selectedMark.id}</h2>
+            <p>assenger Count: {selectedMark.passenger_count}</p>
+            <p>Trip Distance: {selectedMark.trip_distance}</p>
+            <p>Total Amount: {selectedMark.total_amount}</p>
+        </div>
+          }
         </InfoWindow>
       )}
     </GoogleMap>
@@ -92,7 +107,7 @@ export default function App() {
   return (
     <div style={{width: '100vw', height: '100vh'}}>
       <WrappedMap 
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDJzAI78dN-O6L8XUyVnAbCoEkiDF-rPSk`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
         loadingElement={<div style={{height:"100%"}}/>} 
         containerElement={<div style={{height:"100%"}}/>} 
         mapElement={<div style={{height:"100%"}}/>} 
--- src/NYCTripsReader.js
+++ src/NYCTripsReader.js
@@ -1,32 +1,22 @@
-import React, { useState, useEffect } from "react";
 
 export default function NYCDataReader(isGroup, swPosition, nePosition, positionType)
-{   var result = [];
-    //const [data, setData] = useState([]);
-    
-    //useEffect(() => {
-        var startPosition = swPosition;
-        var offset = {
-            lat_offset : nePosition.lat - swPosition.lat,
-            lng_offset : nePosition.lng - swPosition.lng
-        };
-        var params = startPosition.lng + "/" + offset.lng_offset + "/" + startPosition.lat + "/" + offset.lat_offset + "/" + positionType;
-        console.log(params);
-        var url = isGroup? "https://localhost:5001/api/TaxiTrips/groups/" +  params : "https://localhost:5001/api/TaxiTrips/" + params;
-        console.log(url);
-        fetch(url,
-             {
-                 method:"get",
-                 headers:{
-                    'Accept': 'application/json',
-                    'crossDomain':'true',
-                    'Content-Type': 'application/json',
-                    'Pragma': 'no-cache'
-                }
-             })
-        .then(response => response.json())
-        .then(data => result = data);
-      //});
-      console.log(result);
-    return result;
+{   
+    var startPosition = swPosition;
+          var offset = {
+              lat_offset : nePosition.lat - swPosition.lat,
+              lng_offset : nePosition.lng - swPosition.lng
+          };
+          var params = startPosition.lng + "/" + offset.lng_offset + "/" + startPosition.lat + "/" + offset.lat_offset + "/" + positionType;
+          var url = isGroup? `${process.env.REACT_APP_API_URL}/TaxiTrips/groups/` +  params : `${process.env.REACT_APP_API_URL}/TaxiTrips/` + params;
+          return fetch(url,
+               {
+                   method:"get",
+                   headers:{
+                      'Accept': 'application/json',
+                      'crossDomain':'true',
+                      'Content-Type': 'application/json',
+                      'Pragma': 'no-cache'
+                  }
+               })
+          .then(response => response.json());
 }
